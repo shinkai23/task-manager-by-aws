240eea1052f27ac5df52dc2b3fcc0bffd535c32b
オートれん <mamedas.hobby55@gmail.com>
2026-04-29 14:18:19 +0900

    chore: 環境変数を.envで管理するよう修正

--- app/utils/security.py
+++ app/utils/security.py
@@ -1,10 +1,14 @@
 from datetime import datetime, timedelta
 from jose import jwt
 from passlib.context import CryptContext
+import os
+from dotenv import load_dotenv
 
-SECRET_KEY = "secret" #あとでenvに移す！！！
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MININUTES = 60
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY") #あとでenvに移す！！！ <- envに移行済み 26/04/29
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MININUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MININUTES")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
