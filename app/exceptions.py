fd1612cb11259b119e0f315c79a44d891bcc8a4e
オートれん <mamedas.hobby55@gmail.com>
2026-05-05 12:49:07 +0900

    fix: アプリ共通例外の初期化処理を修正

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,19 +1,19 @@
 from fastapi import HTTPException
 
 class AppException(HTTPException):
-    def __intit_(self, status_code: int, code: str, message: str):
+    def __init__(self, status_code: int, code: str, message: str):
         self.code = code
         self.message = message
         super().__init__(status_code=status_code, detail=message)
 
-class NotFoundException(HTTPException):
+class NotFoundException(AppException):
     def __init__(self, message="Resource not found"):
-        super.__init__(404, "NOT_FOUND", message)
+        super().__init__(404, "NOT_FOUND", message)
 
-class UnauthorizedException(HTTPException):
+class UnauthorizedException(AppException):
     def __init__(self, message="Unauthorized"):
         super().__init__(401, "UNAUTHORIZED", message)
 
-class BadRequestException(HTTPException):
+class BadRequestException(AppException):
     def __init__(self, message="Bad request"):
-        super().__init__(400, "BAD_REQUEST0", message)
+        super().__init__(400, "BAD_REQUEST", message)
